07674121ec6976561bf639fd0803aef9aab08c06
TomandJerrytgl <thegreatlordtmj@gmail.com>
2026-04-14 10:50:00 -0700

    end

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,2 +1,14 @@
+"""
 #e^2/4*pi*epslon0*b_0=m_e*v_e^2
-#
+lambda: mean free path
+sigma: cross section area
+n_n: density of neutral particles
+collision frequency: nu=sigma*n_n*v_average
+v_average: mean velocity of electrons
+b: impact parameter
+effection cross section: sigma=pi*b_0^2
+nu_large_angle: nu_large_angle=sigma*n_n*v_average=n_i*z^4*e^4/16*pi*epsilon0^2*m_e^2*v_e^3
+nu propotion to T_e^-3/2
+
+"""
+
