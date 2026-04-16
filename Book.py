a99aa974da08671b1a5438e5598a882f7c31166a
TomandJerrytgl <thegreatlordtmj@gmail.com>
2026-04-15 19:35:59 -0700

    book charpter 1

---
+++ Book.py
@@ -0,0 +1,22 @@
+#Plasma book
+"""
+n_i: ionization particle
+n_n: neutral particle
+n_i/n_n=2.4*10^21*T^1.5*exp(-E_ionization/(K*T))/n_i
+n_i=sqrt(2.4*10^21*T^1.5*exp(-E_ionization/(K*T))*n_n)
+
+Temperature, thermal distribution
+1d
+f(v):frequency distribution of velocity
+f(v)=A*exp(-m*v^2/2*K*T)
+A=n*(m/(2*pi*K*T))^0.5
+
+3d
+f(u,v,w):frequency distribution of velocity
+A_3=n*(m/(2*pi*K*T))^1.5
+
+
+
+
+
+"""
